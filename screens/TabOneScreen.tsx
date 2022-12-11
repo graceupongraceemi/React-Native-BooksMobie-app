b7cf3c17cc2737f1d325ed5eb4833a785676a84c
Emiko Esther S <graceupongraceemi@gmail.com>
2022-12-10 19:57:13 -0800

    In TabOneScreen.tsx styles and graphQL query added

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,10 +1,71 @@
 import { StyleSheet } from 'react-native';
 
-import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
+import { gql, useQuery } from '@apollo/client';
+
+const query = gql`
+  query SearchBooks($q: String) {
+    googleBooksSearch(q: $q, country: "US") {
+      items {
+        id
+        volumeInfo {
+          authors
+          averageRating
+          description
+          imageLinks {
+            thumbnail
+          }
+          title
+          subtitle
+          industryIdentifiers {
+            identifier
+            type
+          }
+        }
+      }
+    }
+    openLibrarySearch(q: $q) {
+      docs {
+        author_name
+        title
+        cover_edition_key
+        isbn
+      }
+    }
+  }
+`;
+
+export default function TabOneScreen({
+  navigation
+}: RootTabScreenProps<'TabOne'>) {
+  const { data, loading, error } = useQuery(query, {
+    variables: { q: 'React Native' }
+  });
+
+  console.log(JSON.stringify(data, null, 2));
+
+  return (
+    <View style={styles.container}>
+      {loading && <ActivityIndicator />}
+      {error && (
+        <View style={styles.container}>
+          <Text style={styles.title}>Error fetching books</Text>
+          <Text>{error.message}</Text>
+        </View>
+      )}
+    </View>
+  );
+}
 
 export default function TabOneScreen() {
+  const { data, loading, error } = useQuery(query, {
+    variables: { q: 'React Native' }
+  });
+
+  console.log(data);
+  console.log(loading);
+  console.log(error);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Hellow world</Text>
